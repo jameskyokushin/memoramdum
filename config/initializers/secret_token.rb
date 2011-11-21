f5543b87d116f3fbf1e59eb45dd7c2f4b33d596b
jameskyokushin <james@kyokushin.com.ph>
2011-11-21 15:26:10 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memoramdum::Application.config.secret_token = '8b13fb0ae8b1fb57c2e6b78d7368f48ade6a5e9381d10bbc91574e0dc49b846df8f69c75376707ddf22e326a3a518af74836caebeb241987b7580c620dcd3b68'
