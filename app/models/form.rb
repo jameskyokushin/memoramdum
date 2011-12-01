1059231e38cdba57da50927c8f8d5093ba0b4c6f
jameskyokushin <james@kyokushin.com.ph>
2011-12-01 14:25:51 +0800

    migrate status/edit form and show

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,23 @@
 class Form < ActiveRecord::Base
+  
+  STATUS_NOTANSWER = 'NOT ANSWER'
+  STATUS_DONE  = 'DONE'
+  STATUS_SENTTOEMAIL = 'SENT TO EMAIL'
+
+ class << self
+      def status_collection
+       {
+        "NOT ANSWER" => STATUS_NOTANSWER,
+        "FOR COLLECTION" => STATUS_SENTTOEMAIL,
+        "DONE" => STATUS_DONE,
+       }
+      end
+   end
+    def status_tag
+        case self.status
+          when STATUS_NOTANSWER then :error
+          when STATUS_COLLECTION then :warning
+          when STATUS_DONE then :ok
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@
 ActiveRecord::Schema.define(:version => 20111123021715) do
 
   create_table "active_admin_comments", :force => true do |t|
-    t.integer  "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "namespace"
+    t.integer   "resource_id",   :null => false
+    t.string    "resource_type", :null => false
+    t.integer   "author_id"
+    t.string    "author_type"
+    t.text      "body"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "namespace"
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
@@ -29,50 +29,50 @@ ActiveRecord::Schema.define(:version => 20111123021715) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "forms", :force => true do |t|
-    t.date     "date_issued"
-    t.string   "issued_to"
-    t.string   "from"
-    t.string   "subject"
-    t.text     "why"
-    t.text     "answer"
-    t.string   "sign_by"
-    t.string   "issued_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "status"
+    t.date      "date_issued"
+    t.string    "issued_to"
+    t.string    "from"
+    t.string    "subject"
+    t.text      "why"
+    t.text      "answer"
+    t.string    "sign_by"
+    t.string    "issued_by"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "status"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
