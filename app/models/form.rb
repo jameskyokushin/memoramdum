e84104c5d022be6619143e313b6cda5d0d890a30
jameskyokushin <james@kyokushin.com.ph>
2011-12-20 11:50:02 +0800

    change sometext

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,13 +2,14 @@ class Form < ActiveRecord::Base
   
   STATUS_NOTANSWER = 'NOT ANSWER'
   STATUS_DONE  = 'DONE'
-  STATUS_SENTTOEMAIL = 'SENT TO EMAIL'
-
+  STATUS_MAILTO = 'SENT TO EMAIL'
+  
+  
  class << self
       def status_collection
        {
         "NOT ANSWER" => STATUS_NOTANSWER,
-        "FOR COLLECTION" => STATUS_SENTTOEMAIL,
+        "SENT TO EMAIL" => STATUS_MAILTO,
         "DONE" => STATUS_DONE,
        }
       end
@@ -16,7 +17,7 @@ class Form < ActiveRecord::Base
     def status_tag
         case self.status
           when STATUS_NOTANSWER then :error
-          when STATUS_COLLECTION then :warning
+          when STATUS_MAILTO then :warning
           when STATUS_DONE then :ok
         end
     end
